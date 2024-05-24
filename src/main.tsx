172a74fb66d8b2364bc8699b7f1ca010f9139f33
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2024-05-24 00:24:09 -0300

    fix rendering

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ActiveMarkerProvider } from "./context/marker-context.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ActiveMarkerProvider>
+        <App />
+      </ActiveMarkerProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
